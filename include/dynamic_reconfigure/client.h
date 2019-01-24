154469c5e4b4f5e3a32734050895ba73b8b98806
James Xu <kejia.xu@microsoft.com>
2019-01-23 19:33:14 -0800

    revert msg variable name change (#7)

--- include/dynamic_reconfigure/client.h
+++ include/dynamic_reconfigure/client.h
@@ -224,7 +224,7 @@ class Client {
                            const ros::Duration& timeout = ros::Duration(0)) {
     ConfigDescription answer;
     if (getDescription(answer, timeout)) {
-      configuration.__fromMessage__(answer.min_value);
+      configuration.__fromMessage__(answer.min);
       return true;
     } else {
       return false;
@@ -242,7 +242,7 @@ class Client {
                            const ros::Duration& timeout = ros::Duration(0)) {
     ConfigDescription answer;
     if (getDescription(answer, timeout)) {
-      configuration.__fromMessage__(answer.max_value);
+      configuration.__fromMessage__(answer.max);
       return true;
     } else {
       return false;
